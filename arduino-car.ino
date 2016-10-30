e1b28454a70c547e0afcbb4d4a54db0e58d577f1
Diego Lopes <dihgg@dihgg.com>
2016-10-30 17:30:36 -0200

    atualização para dois sensores

--- arduino-car.ino
+++ arduino-car.ino
@@ -3,21 +3,17 @@
 
 // CONSTANTS
 const int ratio = 600; // Valor padrão do "preto"
-const int lost = 500; // tempo para considerar a linha como perdida
 
 //VARIABLES
-Infrared irL, irC, irR; //Sensores infravermelhos
+Infrared irL, irR; //Sensores infravermelhos
 Motors motors; //Controlador do motor
-String actual = "none"; //Controlar qual o comando atual
-String last = "none"; //Controlar qual o último comando
-int lostTimer = lost; //"timer" de linha linha perdida
+String last = "none";
 
 // SETUP
 void setup() {
   //Configurar os sensores
   irL.setup(A0); // Esquerdo
-  irC.setup(A1); // Central
-  irR.setup(A2); // Direito
+  irR.setup(A1); // Direito
 
   //Configurar o motor
   motors.setup();
@@ -27,48 +23,29 @@ void setup() {
 
 // SETUP
 void loop() {
-  // Checar sensores
-  checkSensors();
-  if(actual != "none") {
-    // Se algum sensor está vendo a linha
-    motors.command(actual);
-  } else {
-    // Se nenhum sensor está vendo a linha
-    if(lostTimer > 0){
-      Serial.println(lostTimer);
-      // iniciar o lostTimer
-      lostTimer--;
-        // Se houver um último comando
-      if(last != "none") {
-           motors.command(last);
-      } else {
-        motors.go();
-      }
+  if(irL.is(ratio) && irR.is(ratio)) {
+    motors.go();
+    int rdn = (rand() % 100 + 1);
+    if(rdn<=50) {
+      last = "left";
     } else {
-      if(last == "go") {
-        motors.left(true);
-      }
+      last = "right";
+    }
+    
+  } else if(irL.is(ratio)) {
+    motors.left();
+    last = "left";
+  }else if(irR.is(ratio)) {
+    motors.right();
+    last = "right";
+  } else {
+    if(last == "left") {
+      motors.left(true);
+    } else if(last == "right") {
+      motors.right(true);
+    } else {
+      motors.go();
     }
   }
-  delay(1);
 }
-
-void checkSensors() {
-  if(irC.is(ratio)) { 
-    actual = "go";
-    last = "go";
-    lostTimer = lost;
-  } else if(irL.is(ratio)) {
-    actual = "left";
-    last = "left";
-    lostTimer = lost;
-  } else if(irR.is(ratio)) {
-    actual = "right";
-    last = "right";
-    lostTimer = lost;
-  } else {
-    actual = "none";
-  }
-}
-
 
